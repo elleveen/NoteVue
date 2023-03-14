ff81a5416c844584291c9237d6d798b0e1a4a7d6
gr411_maean <eleonoramanushkova@yandex.ru>
2023-03-14 12:56:04 +0700

    add in 1 column

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
+let eventBus = new Vue()
+
 Vue.component('column', {
 
     template: `
  
         <div class="columns">
             <add_task></add_task>
+            <p class="error" v-for="error in errors">{{ error }}</p>
             <div class="list">
                 <column_1 :column_1="column_1"></column_1>
                 <column_2 :column_2="column_2"></column_2>
@@ -18,16 +21,29 @@ Vue.component('column', {
             column_1: [],
             column_2: [],
             column_3: [],
+            errors: [],
         }
 
     },
+    mounted() {
+        eventBus.$on('addColumn_1', ColumnCard => {
+
+            if (this.column_1.length < 3) {
+                this.errors.length = 0
+                this.column_1.push(ColumnCard)
+            } else {
+                this.errors.length = 0
+                this.errors.push()
+            }
+        })
+    }
 })
 
 Vue.component('add_task', {
     template: `
     <section id="main" class="main-alt">
     
-        <form class="row">
+        <form class="row" @submit.prevent="Submit">
         <div class="form_control">
             <div class="form_name">
                 <input required type="text" v-model="name" id="name" placeholder="Введите название заметки"/>
@@ -60,19 +76,74 @@ Vue.component('add_task', {
             date: null,
         }
     },
+    methods: {
+
+        Submit() {
+            let card = {
+                name: this.name,
+                points: [
+                    {name: this.point_1, completed: false},
+                    {name: this.point_2, completed: false},
+                    {name: this.point_3, completed: false},
+                    {name: this.point_4, completed: false},
+                    {name: this.point_5, completed: false}
+                ],
+                date: null,
+                status: 0,
+                errors: [],
+            }
+            eventBus.$emit('addColumn_1', card)
+            this.name = null;
+            this.point_1 = null
+            this.point_2 = null
+            this.point_3 = null
+            this.point_4 = null
+            this.point_5 = null
+        }
+    }
 
 })
 
 Vue.component('column_1', {
+    props: {
+        column_1: {
+            type: Array,
+        },
+        column_2: {
+            type: Array,
+        },
+        card: {
+            type: Object,
+        },
+        errors: {
+            type: Array,
+        },
+    },
     template: `
         <section id="main" class="main-alt">
             <div class="column column_one">
             <p>Задачи</p>
-            
+            <div class="card" v-for="card in column_1">
+                <h3>{{ card.name }}</h3>
+                    <ul class="tasks" v-for="task in card.points"
+                        v-if="task.name != null"
+                        @click="TaskCompleted(card, task)"
+                        :class="{completed: task.completed}">
+                        <li>
+                        {{ task.name }}
+                        </li>
+                    </ul>
+                    
+            </div> 
             </div>
         </section>
     `,
+
+    methods: {
+
+    },
 })
+
 
 Vue.component('column_2', {
     template: `
